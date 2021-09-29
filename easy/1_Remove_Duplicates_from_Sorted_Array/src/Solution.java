c8616b9852a8fe4a2edd74dd9360dd2df05db945
Radina <radina.nuneva@gmail.com>
2021-09-29 10:21:54 +0300

    Update Solution.java

--- easy/1_Remove_Duplicates_from_Sorted_Array/src/Solution.java
+++ easy/1_Remove_Duplicates_from_Sorted_Array/src/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println(removeDuplicates(null));
+        System.out.println(removeDuplicates(nums));
     }
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
