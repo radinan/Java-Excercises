ce14995d8961ea4fc0bf91c4bdc1bf6b10b472f3
Radina <r_nuneva@abv.bg>
2021-09-29 09:52:32 +0300

    Add gitignore and ex 1

---
+++ easy/1_Remove_Duplicates_from_Sorted_Array/src/Solution.java
@@ -0,0 +1,62 @@
+public class Solution {
+//    Input: nums = [0,0,1,1,1,2,2,3,3,4]
+//    Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(null));
+    }
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int indexLastUnique = 0;
+
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[indexLastUnique] != nums[i]) {
+                indexLastUnique++;
+                nums[indexLastUnique] = nums[i];
+            }
+        }
+
+        return indexLastUnique + 1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
