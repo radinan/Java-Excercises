dcfe9d90c63e64cb81e1e84bb9c19c6a08e7dc20
Radina <r_nuneva@abv.bg>
2021-09-30 13:03:37 +0300

    Add ex5

---
+++ easy/5_Single_Number/src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        //we accept that the length is only odd number
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i+=2) {
+            if (nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+
+        return nums[nums.length -1];
+    }
+}
