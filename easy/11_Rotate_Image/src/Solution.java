62f79789bf8539389fad2f8740061b8f56cc1dea
Radina <radina.nuneva@gmail.com>
2021-10-05 19:10:10 +0300

    !Add ex11 v2

--- easy/11_Rotate_Image/src/Solution.java
+++ easy/11_Rotate_Image/src/Solution.java
@@ -10,23 +10,26 @@ public class Solution {
     }
 
     public static void rotate(int[][] matrix) {
-        int size = matrix.length;
+        transpose(matrix);
+        reflect(matrix);
+    }
 
-        for (int i = 0; i < size / 2; ++i) {
-            for (int j = i; j < size - 1 - i; ++j) {
-                int holder;
+    private static void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = i; j < matrix.length; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
 
-                holder = matrix[j][size - 1 - i];
-                matrix[j][size - 1 - i] = matrix[i][j];
-                matrix[i][j] = holder;
-
-                holder = matrix[size - 1 - i][size - 1 - j];
-                matrix[size - 1 - i][size - 1 - j] = matrix[i][j];
-                matrix[i][j] = holder;
-
-                holder = matrix[size - 1 - j][i];
-                matrix[size - 1 - j][i] = matrix[i][j];
-                matrix[i][j] = holder;
+    private static void reflect(int[][] matrix) {
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix.length / 2; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
             }
         }
     }
