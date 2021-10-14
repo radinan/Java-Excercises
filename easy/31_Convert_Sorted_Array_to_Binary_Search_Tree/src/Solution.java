b7453589fe690e8d1639a069cdda1b2e37bba1a2
Radina <radina.nuneva@gmail.com>
2021-10-14 22:07:37 +0300

    Add ex31 v2

--- easy/31_Convert_Sorted_Array_to_Binary_Search_Tree/src/Solution.java
+++ easy/31_Convert_Sorted_Array_to_Binary_Search_Tree/src/Solution.java
@@ -3,18 +3,17 @@ import java.util.Set;
 
 public class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
-        return buildTree(nums, 0, nums.length - 1, new HashSet<Integer>());
+        return buildTree(nums, 0, nums.length - 1);
     }
 
-    private TreeNode buildTree(int[] nums, int start, int end, Set<Integer> visited) {
-        if (visited.contains(start) || visited.contains(end)) return null;
+    private TreeNode buildTree(int[] nums, int start, int end) {
+        if (start > end) return null;
 
         int mid = start + (end - start + 1) / 2;
-        visited.add(mid);
 
         TreeNode root = new TreeNode(nums[mid]);
-        root.left = buildTree(nums, start, mid - 1, visited);
-        root.right = buildTree(nums, mid + 1, end, visited);
+        root.left = buildTree(nums, start, mid - 1);
+        root.right = buildTree(nums, mid + 1, end);
         return root;
     }
 }
