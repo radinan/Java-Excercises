2d3574ac01e5cd02a68283441a91d6f5030d475f
Radina <radina.nuneva@gmail.com>
2021-10-20 14:01:24 +0300

    Add ex32 v2

--- easy/32_Merge_Sorted_Array/src/Solution.java
+++ easy/32_Merge_Sorted_Array/src/Solution.java
@@ -1,13 +1,21 @@
-import java.util.Arrays;
-
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         if (n == 0) return;
 
-        for (int i = m; i < nums1.length; i++) {
-            nums1[i] = nums2[i - m];
+        int i = m - 1,
+            j = n - 1,
+            k = m + n - 1;
+
+        while (j >= 0 && i >= 0) {
+            if (nums1[i] < nums2[j]) {
+                nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
+            }
         }
 
-        Arrays.sort(nums1);
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
     }
 }
